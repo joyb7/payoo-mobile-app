06f9f08a4aaf167972280801969658985092e3f9
Joy Barua <joybarua1252002@gmail.com>
2025-08-27 21:22:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// login button functionality
+
+document.getElementById('loginButton').addEventListener('click', function(event) {
+    event.preventDefault() // Prevent form submission
+    const mobileNumber = 123456789
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById('mobile-number').value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNumberValueConverted, pinNumberValueConverted)
+
+    if (mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber) {
+        alert('Login successful!')
+        window.location.href = './main.html'
+    } 
+    else {
+        alert('Login failed!')
+    }
+})
